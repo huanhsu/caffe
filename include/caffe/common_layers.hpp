c28a2d75751fc70309bfc54f66d620d3e4885b9f
Cysu <st.cysu@gmail.com>
2015-07-02 21:32:48 +0800

    A layer gathering output blobs from all MPI processes

--- include/caffe/common_layers.hpp
+++ include/caffe/common_layers.hpp
@@ -606,6 +606,31 @@ class SliceLayer : public Layer<Dtype> {
   vector<int> slice_point_;
 };
 
+/**
+ * @brief Gather bottom blob from MPI processes and concatenate them in rank
+ *        order as the output.
+ *
+ * TODO(dox): thorough documentation for Forward, Backward, and proto params.
+ */
+template <typename Dtype>
+class MPIGatherLayer : public Layer<Dtype> {
+ public:
+  explicit MPIGatherLayer(const LayerParameter& param)
+      : Layer<Dtype>(param) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+
+  virtual inline const char* type() const { return "MPIGather"; }
+
+ protected:
+  virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_cpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+};
+
 }  // namespace caffe
 
 #endif  // CAFFE_COMMON_LAYERS_HPP_
--- include/caffe/util/mpi_templates.hpp
+++ include/caffe/util/mpi_templates.hpp
@@ -1,6 +1,6 @@
+#ifdef USE_MPI
 #ifndef MPI_TEMPLATES_HPP_
 #define MPI_TEMPLATES_HPP_
-#ifdef USE_MPI
 
 #include "mpi.h"
 
@@ -35,5 +35,37 @@ inline int MPIAllreduce<double>(int count, void* sendbuf, void* recvbuf,
   return MPI_Allreduce(sendbuf, recvbuf, count, MPI_DOUBLE, op, comm);
 }
 
-#endif // USE_MPI
+template <typename Dtype>
+inline int MPIGather(int count, const void* sendbuf, void* recvbuf,
+                     int root = 0, MPI_Comm comm = MPI_COMM_WORLD);
+template <>
+inline int MPIGather<float>(int count, const void* sendbuf, void* recvbuf,
+                            int root, MPI_Comm comm) {
+  return MPI_Gather(sendbuf, count, MPI_FLOAT, recvbuf, count, MPI_FLOAT,
+      root, comm);
+}
+template <>
+inline int MPIGather<double>(int count, const void* sendbuf, void* recvbuf,
+                             int root, MPI_Comm comm) {
+  return MPI_Gather(sendbuf, count, MPI_DOUBLE, recvbuf, count, MPI_DOUBLE,
+      root, comm);
+}
+
+template <typename Dtype>
+inline int MPIScatter(int count, const void* sendbuf, void* recvbuf,
+                      int root = 0, MPI_Comm comm = MPI_COMM_WORLD);
+template <>
+inline int MPIScatter<float>(int count, const void* sendbuf, void* recvbuf,
+                             int root, MPI_Comm comm) {
+  return MPI_Scatter(sendbuf, count, MPI_FLOAT, recvbuf, count, MPI_FLOAT,
+      root, comm);
+}
+template <>
+inline int MPIScatter<double>(int count, const void* sendbuf, void* recvbuf,
+                             int root, MPI_Comm comm) {
+  return MPI_Scatter(sendbuf, count, MPI_DOUBLE, recvbuf, count, MPI_DOUBLE,
+      root, comm);
+}
+
 #endif // MPI_TEMPLATES_HPP_
+#endif // USE_MPI
