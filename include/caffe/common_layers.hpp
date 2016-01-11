f20e5174ecb96c4c6228cfeb9af8659b954d856f
Cysu <st.cysu@gmail.com>
2016-01-11 15:20:04 +0800

    Fix the bug that BN running mean and std have weight decay by default

--- include/caffe/common_layers.hpp
+++ include/caffe/common_layers.hpp
@@ -13,6 +13,10 @@
 #include "caffe/neuron_layers.hpp"
 #include "caffe/proto/caffe.pb.h"
 
+#ifdef USE_CUDNN
+#include "caffe/util/cudnn.hpp"
+#endif
+
 namespace caffe {
 
 /**
@@ -640,7 +644,6 @@ class BNLayer : public Layer<Dtype> {
   void BroadcastChannel(const Dtype* input, Dtype* output);
 
   bool frozen_;
-  bool moving_average_;
   Dtype bn_momentum_;
   Dtype bn_eps_;
 
@@ -654,11 +657,52 @@ class BNLayer : public Layer<Dtype> {
   Blob<Dtype> batch_statistic_;
 
   Blob<Dtype> x_norm_;
-  Blob<Dtype> x_std_;
+  Blob<Dtype> x_inv_std_;
 
   Blob<Dtype> spatial_sum_multiplier_;
   Blob<Dtype> batch_sum_multiplier_;
 };
+
+#if defined(USE_CUDNN)
+#if CUDNN_VERSION_MIN(4, 0, 0)
+/**
+ * @brief cuDNN implementation of BNLayer.
+ *        Fallback to BNLayer for CPU mode.
+ */
+template <typename Dtype>
+class CuDNNBNLayer : public BNLayer<Dtype> {
+ public:
+  explicit CuDNNBNLayer(const LayerParameter& param)
+      : BNLayer<Dtype>(param), handles_setup_(false) {}
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Reshape(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual ~CuDNNBNLayer();
+
+  virtual inline const char* type() const { return "BN"; }
+  virtual inline int ExactNumBottomBlobs() const { return 1; }
+  virtual inline int ExactNumTopBlobs() const { return 1; }
+
+ protected:
+  virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
+      const vector<Blob<Dtype>*>& top);
+  virtual void Backward_gpu(const vector<Blob<Dtype>*>& top,
+      const vector<bool>& propagate_down, const vector<Blob<Dtype>*>& bottom);
+
+  bool handles_setup_;
+  cudnnHandle_t handle_;
+  cudnnTensorDescriptor_t bottom_desc_;
+  cudnnTensorDescriptor_t top_desc_;
+  cudnnTensorDescriptor_t bn_param_desc_;
+
+  Blob<Dtype> scale_buf_;
+  Blob<Dtype> bias_buf_;
+  Blob<Dtype> save_mean_;
+  Blob<Dtype> save_inv_variance_;
+};
+#endif
+#endif
 
 }  // namespace caffe
 
